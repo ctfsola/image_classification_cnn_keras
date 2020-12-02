dd198fce4557718ccbf4b4572e08e494424386ec
穿雨 <chentianfa@91yu.com>
2020-12-02 22:08:41 +0800

    训练脚本删除一些多余代码

--- train.py
+++ train.py
@@ -68,14 +68,12 @@ x = Activation('relu')(x)
 
 
 
-
 x = Flatten()(x)
 x = Dropout(0.5)(x)
 x = Dense(256, activation='relu')(x)
 x = Dense(num_classes, activation='softmax')(x)
 
 model = Model(img_input, x)
-
 
 
 
@@ -109,25 +107,6 @@ model = Model(img_input, x)
 # model = Model(img_input, output)
 
 
-
-
-# model = Sequential()
-# model.add(layers.SeparableConv2D(32, 3,
-#                                  activation='relu',
-#                                  input_shape=(height, width, channels,)))
-# model.add(layers.SeparableConv2D(64, 3, activation='relu'))
-# model.add(layers.MaxPooling2D(2))
-
-# model.add(layers.SeparableConv2D(64, 3, activation='relu'))
-# model.add(layers.SeparableConv2D(128, 3, activation='relu'))
-# model.add(layers.MaxPooling2D(2))
-
-# model.add(layers.SeparableConv2D(64, 3, activation='relu'))
-# model.add(layers.SeparableConv2D(128, 3, activation='relu'))
-# model.add(layers.GlobalAveragePooling2D())
-
-# model.add(layers.Dense(32, activation='relu'))
-# model.add(layers.Dense(num_classes, activation='softmax'))
 
 
 #model.compile(optimizer='rmsprop', loss= tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True) ,metrics=['acc']) # 整数标签要用 sparse_categorical_crossentropy
